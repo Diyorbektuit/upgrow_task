905f4aef6b88b935dc446f95ab33f9cbab8f28df
Diyorbektuit <d.xazratqulov@tuit.uz>
2025-01-28 21:34:04 +0500

    static

--- social_auth/utils.py
+++ social_auth/utils.py
@@ -1,7 +1,6 @@
 import uuid
-
+from django.db import IntegrityError
 from .models import User
-import uuid
 
 
 def create_user_or_update(user_data, via):
@@ -13,6 +12,7 @@ def create_user_or_update(user_data, via):
     else:
         user = User.objects.filter(facebook=facebook)
         auth_type = "via_facebook"
+
     if user.exists():
         user.update(
             auth_type=auth_type,
@@ -23,25 +23,38 @@ def create_user_or_update(user_data, via):
             "refresh": user.first().tokens()["refresh"],
             "first": 0
         }
-    username = uuid.uuid4()
-    if via == "email":
+
+    username = f"{email or facebook}-{uuid.uuid4()}"
+    try:
+        if via == "email":
+            user = User.objects.create_user(
+                email=email,
+                password=str(uuid.uuid4())[:8],
+                auth_type=auth_type,
+                username=username,
+            )
+        else:
+            user = User.objects.create_user(
+                facebook=facebook,
+                password=str(uuid.uuid4())[:8],
+                auth_type=auth_type,
+                username=username,
+            )
+        user.save()
+    except IntegrityError:
+        username = f"{email or facebook}-{uuid.uuid4()}"
         user = User.objects.create_user(
             email=email,
-            password=str(uuid.uuid4())[:8],
-            auth_type=auth_type,
-            username=f"{email}-{username}",
-        )
-    else:
-        user = User.objects.create_user(
             facebook=facebook,
             password=str(uuid.uuid4())[:8],
             auth_type=auth_type,
-            username=f"{facebook}-{username}",
+            username=username,
         )
-    user.save()
+        user.save()
+
     return {
-            "success": True,
-            "access": user.tokens()["access"],
-            "refresh": user.tokens()["refresh"],
-            "first": 1
-             }
+        "success": True,
+        "access": user.tokens()["access"],
+        "refresh": user.tokens()["refresh"],
+        "first": 1
+    }
